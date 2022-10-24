a639ee12dda2a85bed339eb07c9a5724ce8de084
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-24 12:06:54 +0200

    setup one-to-many relations

--- PokemonReviewApp/Models/Country.cs
+++ PokemonReviewApp/Models/Country.cs
@@ -6,5 +6,6 @@ namespace PokemonReviewApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
     }
 }
--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -8,5 +8,6 @@ namespace PokemonReviewApp.Models
         public string Name { get; set; }
 
         public string Gym { get; set; }
+        public Country Country { get; set; }
     }
 }
