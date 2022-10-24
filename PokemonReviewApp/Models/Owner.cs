482c67f9ce9fff076243b4e5a80f1d66c8b39437
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-24 11:28:33 +0200

    created owner and country models

---
+++ PokemonReviewApp/Models/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace PokemonReviewApp.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Gym { get; set; }
+    }
+}
